8b12bd2106fd0f359cbf13680025370ff02db27d
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-03-28 21:28:30 +0200

    Register implemented/ all working

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const { listenerCount } = require('stream');
-const bcrypt = require('bcrypt-nodejs');
+//const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 
 const app = express();
@@ -27,13 +27,6 @@ const database = {
             entries: 0,
             joined: new Date()
         }
-    ],
-    login: [ 
-        {
-            id: '987',
-            hash: '',
-            email: 'john@gmail.com'
-        }
     ]
 }
 
@@ -42,14 +35,15 @@ app.get('/', (req,res)=>{
 })
 
 app.post('/signin', (req,res) =>{
-
-
-    if (req.body.email === database.users[0].email && req.body.password === database.users[0].password){
-        res.json('success');
+    if (req.body.email === database.users[0].email && 
+        req.body.password === database.users[0].password){
+        // Return the user 0 from database: 
+        res.json(database.users[0]);
     } else {
-        res.status(400).json('error logging in ')
+        res.status(400).json('error logging in '),
+        console.log('something went wrong')
     }
-    res.json('signin working');
+    //res.json('signin working');
 })
 
 app.post('/register', (req, res)=>{
@@ -94,7 +88,21 @@ app.put('/image', (req,res)=>{
     }
 })
 
+app.listen(3000, ()=> {
+    console.log('app is runing on port 3000');
+});
 
+
+
+/* STEPS TODO
+/sign in --> POST = success or fail
+/register --> POST = new user object to return
+/profile/: userID --> GET = user
+/image --> PUT --> user[count] (for ranking of the profiles against each other )
+*/
+
+
+//encription with hash
 
 // bcrypt.hash("bacon", null, null, function(err, hash) {
 //     // Store hash in your password DB.
@@ -109,18 +117,3 @@ app.put('/image', (req,res)=>{
 // });
 
 
-
-
-
-app.listen(3004, ()=> {
-    console.log('app is runing on port 3004');
-});
-
-
-
-/* STEPS TODO
-/sign in --> POST = success or fail
-/register --> POST = new user object to return
-/profile/: userID --> GET = user
-/image --> PUT --> user[count] (for ranking of the profiles against each other )
-*/
