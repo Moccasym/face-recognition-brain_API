06816b26f2bd7ee870b7390b2729f3a06d9bd330
Moccasym <57842536+Moccasym@users.noreply.github.com>
2023-03-30 20:25:22 +0200

    included Rank

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.get('/profile/:id', (req,res)=>{
 });
 
 app.put('/image', (req,res)=>{
-     const { id } = req.body;
+    const { id } = req.body;
     let found = false;
     database.users.forEach(user => {
         if (user.id === id){
